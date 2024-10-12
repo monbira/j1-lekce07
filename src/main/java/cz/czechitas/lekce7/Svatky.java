75c3d7bb6323299cb54186eb4e42423899593fae
monikabiras <monikabiras@gmail.com>
2024-10-12 19:56:06 +0200

    po odkomentování zbytku kódu funkční

--- src/main/java/cz/czechitas/lekce7/Svatky.java
+++ src/main/java/cz/czechitas/lekce7/Svatky.java
@@ -2,6 +2,7 @@ package cz.czechitas.lekce7;
 
 import java.time.Month;
 import java.time.MonthDay;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -59,8 +60,7 @@ public class Svatky {
    * @return Den a měsíc, případně {@code null}, pokud jméno nebylo nalezeno.
    */
   public MonthDay kdyMaSvatek(String jmeno) {
-    //TODO
-    return null;
+      return svatky.getOrDefault(jmeno, null);
   }
 
   /**
@@ -69,8 +69,7 @@ public class Svatky {
    * @return {@code true}, pokud je jméno v seznamu. Jinak vrací {@code false}.
    */
   public boolean jeVSeznamu(String jmeno) {
-    //TODO
-    return false;
+    return svatky.containsKey(jmeno);
   }
 
   /**
@@ -78,8 +77,7 @@ public class Svatky {
    * @return
    */
   public int getPocetJmen() {
-    //TODO
-    return 0;
+    return svatky.size();
   }
 
   /**
@@ -87,8 +85,7 @@ public class Svatky {
    * @return Neseřazený seznam jmen.
    */
   public Set<String> getSeznamJmen() {
-    //TODO
-    return null;
+    return svatky.keySet();
   }
 
   /**
@@ -97,7 +94,7 @@ public class Svatky {
    * @param denMesic Den a měsíc, kdy má dané jméno svátek.
    */
   public void pridatSvatek(String jmeno, MonthDay denMesic) {
-    //TODO
+    svatky.put(jmeno, denMesic);
   }
 
   /**
@@ -107,7 +104,7 @@ public class Svatky {
    * @param mesic Měsíc, kdy má dané jméno svátek (1–12).
    */
   public void pridatSvatek(String jmeno, int den, int mesic) {
-    //TODO
+    svatky.put(jmeno, MonthDay.of(mesic, den));
   }
 
   /**
@@ -117,7 +114,7 @@ public class Svatky {
    * @param mesic Měsíc, kdy má dané jméno svátek.
    */
   public void pridatSvatek(String jmeno, int den, Month mesic) {
-    //TODO
+    svatky.put(jmeno, MonthDay.of(mesic, den));
   }
 
   /**
@@ -125,6 +122,6 @@ public class Svatky {
    * @param jmeno Jméno ke smazání.
    */
   public void smazatSvatek(String jmeno) {
-    //TODO
+    svatky.remove(jmeno);
   }
 }
